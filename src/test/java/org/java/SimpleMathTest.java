665ca53772dfa692db43c118b09020113bc2d31b
deyvisonborges <deyvisonsilvaborges@gmail.com>
2026-01-02 18:01:07 -0300

    chore: add junit life cycle annotations

--- src/test/java/org/java/SimpleMathTest.java
+++ src/test/java/org/java/SimpleMathTest.java
@@ -2,11 +2,32 @@ package org.java;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
 class SimpleMathTest {
+    SimpleMath math;
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("===> Initialize Before All");
+    }
+
+    @AfterAll()
+    static void cleanup() {
+        System.out.println("<=== Finish tests process");
+    }
+
+    @BeforeEach()
+    void before() {
+        this.math = new SimpleMath();
+        System.out.println("Before each");
+    }
+
+    @AfterEach()
+    void after() {
+        System.out.println("After each");
+    }
 
     /**
      * [System Under Test]
@@ -16,7 +37,7 @@ class SimpleMathTest {
     @Test
     @DisplayName("Test if 6.2 + 2 = 8.2")
     void testSum_when_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
-        SimpleMath math = new SimpleMath();
+        System.out.println("Sum method");
         Double actual = math.sum(6.2D, 2D);
         double expected = 8.2D;
         assertEquals(expected, actual, () -> "The testSum() did not produce expect result");
@@ -27,15 +48,17 @@ class SimpleMathTest {
     @Test
     @DisplayName("Subtract values")
     void subtraction() {
-        SimpleMath math  = new SimpleMath();
+        System.out.println("Sum method");
         assertEquals(1.0D, math.subtraction(2.0D, 1.0D), () -> "Fail to subtract values");
     }
 
-    @Test
+    @Test()
     void multiplication() {
+        System.out.println("Sum method");
     }
 
     @Test()
+    @Disabled("Test Disabled")
     void testDivision_When_FirstNumberIsDividedByZero_ShouldThrowArithmeticException() {
         fail();
     }
