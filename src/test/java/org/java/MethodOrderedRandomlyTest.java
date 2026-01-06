74455b09b67f6248707ab07d87c85d8755a57eea
deyvisonborges <deyvisonsilvaborges@gmail.com>
2026-01-06 18:13:35 -0300

    chore: work with test method order

---
+++ src/test/java/org/java/MethodOrderedRandomlyTest.java
@@ -0,0 +1,24 @@
+package org.java;
+
+import org.junit.jupiter.api.*;
+
+// Por padrao , os testes sao renderizados pelo nome
+// Mas posso fazer com que rode aleatoriamente
+//@TestMethodOrder(MethodOrderer.Random.class) // roda a ordem de forma aleatoria - ordenacao randomica
+//@TestMethodOrder(MethodOrderer.MethodName.class) // ordenacao por nome (ja eh padrao, mas estou explicitando)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // ordenacao por indice
+public class MethodOrderedRandomlyTest {
+
+    @Test()
+    @Order(2)
+    void test_B() {}
+
+    @Test()
+    @Order(3)
+    void test_C() {}
+
+    @Test()
+    @Order(1)
+    void test_A() {}
+
+}
