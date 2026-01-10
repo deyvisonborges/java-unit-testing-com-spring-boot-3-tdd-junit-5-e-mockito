3906c997dbde0731be17461fb0015b06a88d73b1
deyvisonborges <deyvisonsilvaborges@gmail.com>
2026-01-10 00:33:26 -0300

    chore: learning mock and when

---
+++ src/test/java/org/java/mockito/stubs/CourseBusinessMockTest.java
@@ -0,0 +1,37 @@
+package org.java.mockito.stubs;
+
+import org.java.mockito.CourseBusiness;
+import org.java.mockito.ICourseService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CourseBusinessMockTest {
+    ICourseService mockService;
+    CourseBusiness business;
+
+    @BeforeEach()
+    void before() {
+//        mockService = mockServicenew CourseServiceStub();
+        // Com mockito
+        mockService = Mockito.mock(ICourseService.class);
+        business = new CourseBusiness(mockService);
+    }
+    @Test
+    void testCourseRelatedToSpring_When_UsingAStub() {
+        List<String> courses = Arrays.asList(
+            "Stub Spring 1",
+            "Stub Spring 2",
+            "Stub Spring 3",
+            "Stub Not Sprin.. 3"
+        );
+        Mockito.when(mockService.retrieveCourse("Spring")).thenReturn(courses);
+        var filters = business.retrieveCoursesRelatedToSpring("Spring");
+        assertEquals(3, filters.size());
+    }
+}
--- src/test/java/org/java/mockito/stubs/CourseBusinessStubTest.java
+++ src/test/java/org/java/mockito/stubs/CourseBusinessStubTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CourseServiceStubTest {
+class CourseBusinessStubTest {
     @Test
     void testCourseRelatedToSpring_When_UsingAStub() {
         CourseServiceStub stub = new CourseServiceStub();
