335262e3b02c9c607a76aafd06c7f2c29d1f84dd
deyvisonborges <deyvisonsilvaborges@gmail.com>
2026-01-30 14:08:08 -0300

    feat: config test containers

---
+++ app-spring/src/test/java/org/java/appspring/AbstractIntegrationTest.java
@@ -0,0 +1,47 @@
+package org.java.appspring;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.28");
+
+        public static void start() {
+            Startables.deepStart(Stream.of(mysql)).join();
+        }
+
+        private static Map<String, String> createConnectionConfiguration() {
+            return Map.of("spring:\r\n"
+            + " datasource:\r\n"
+            + " driver", mysql.getJdbcUrl(),
+
+        "spring:\r\n"
+                + " datasource:\r\n"
+                + " username", mysql.getUsername(),
+
+    "spring:\r\n"
+            + " datasource:\r\n"
+            + " password", mysql.getUsername());
+        }
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            start();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testContainers =
+                new MapPropertySource("test-containers", (Map) createConnectionConfiguration());
+            environment.getPropertySources().addFirst(testContainers);
+        }
+    }
+
+}
---
+++ app-spring/src/test/java/org/java/appspring/PersonIntegrationTest.java
@@ -0,0 +1,9 @@
+package org.java.appspring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PersonIntegrationTest {
+
+}
+
